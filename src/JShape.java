bb7249e58300ee498bb082989a7c863aead2b40d
cokpsz <cokpsz@gmail.com>
2018-07-17 08:04:04 +0700

    Update 11 Juli 2018 #2

---
+++ src/JShape.java
@@ -0,0 +1,16 @@
+import javafx.scene.paint.Paint;
+
+public class JShape extends Shape{
+    public JShape() {
+        super();
+    }
+    @Override
+    public void initShape() {
+        getMinosArray()[0].move(0,0);
+        getMinosArray()[1].move(1,0);
+        getMinosArray()[2].move(2,0);
+        getMinosArray()[3].move(2,1);
+        setRotator(1);
+        setColor(Paint.valueOf("Navy"));
+    }
+}
